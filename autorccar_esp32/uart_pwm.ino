fc30dde98e97828b3e35eba82b1205b96405753e
gspark87 <pks87@nate.com>
2023-01-01 02:11:08 +0900

    added esc pwm

--- autorccar_esp32/uart_pwm.ino
+++ autorccar_esp32/uart_pwm.ino
@@ -16,13 +16,19 @@ int pwm_resolution = 16;  // 16bit
 int dat[3];
 
 void servoWrite(int ch, int deg) {
+  // 0deg(1ms) - 90deg(1.5ms) - 180deg(2ms)
   int duty = deg*18.2 + 3277;
   ledcWrite(ch, duty);
 }
 
+void escWrite(int ch, int val) {
+  // duty : 3277(1ms) - 4915(1.5ms) - 6553(2ms)
+  int duty = val;
+  ledcWrite(ch, duty);
+}
+
 void error_loop(){
-  while(1)
-  {
+  while(1) {
     digitalWrite(Pin_RLED, !digitalRead(Pin_RLED));
     delay(100);
   }
@@ -33,14 +39,14 @@ void setup() {
   Serial.begin(115200);
 
   setCpuFrequencyMhz(80); // Set CPU clock to 80, 160, 240MHz
-  
-  pinMode(Pin_GLED, OUTPUT);  
+
+  pinMode(Pin_GLED, OUTPUT);
   pinMode(Pin_RLED, OUTPUT);
   pinMode(Pin_YLED, OUTPUT);
-  
+
   digitalWrite(Pin_GLED, LOW);
   digitalWrite(Pin_RLED, LOW);
-  digitalWrite(Pin_YLED, LOW);  
+  digitalWrite(Pin_YLED, LOW);
 
   ledcSetup(pwm_ch0, pwm_freq, pwm_resolution);
   ledcSetup(pwm_ch1, pwm_freq, pwm_resolution);
@@ -67,29 +73,29 @@ void loop() {
         for(int i=0; i<3; i++)
           dat[i]= (Serial.read() << 8) | Serial.read();
 
-        xs = dat[0];
-        xa = dat[1];
+        xs = dat[0];  // steering
+        xa = dat[1];  // speed controller
         xm = dat[2];
 
         if (xs >= 180)
           xs = 180;
         if (xs <= 0)
           xs = 0;
-        if (xa >= 180)
-          xa = 180;
-        if (xa <= 0)
-          xa = 0;
- 
+        if (xa >= 6553)
+          xa = 6553;
+        if (xa <= 3277)
+          xa = 3277;
+
         servoWrite(pwm_ch0, xs);  // steer pwm 0-180
-        servoWrite(pwm_ch1, xa);  // accel pwm 0(reverse) 90(static) 180(accel)
+        escWrite(pwm_ch1, xa);    // accel pwm 3277(reverse) 4915(static) 6533(max.accel)
         //servoWrite(pwm_ch2, msg->z);
-      
-        if (xa < 90)  // backward
+
+        if (xm == 2)  // manual
           digitalWrite(Pin_YLED, HIGH);
         else
           digitalWrite(Pin_YLED, LOW);
-      
-        if (xm > 0)  // operating 
+
+        if (xm > 0)  // start command
           digitalWrite(Pin_GLED, HIGH);
         else
           digitalWrite(Pin_GLED, LOW);
